262bfbcd0ddda6d49b3ec0fe93350cdf4a351cb8
Navdeep <rathorenavdeep110@gmail.com>
2023-02-16 22:17:20 +0530

    Learnt how to make patterns in Javascript

--- Practice.js
+++ Practice.js
@@ -16,4 +16,40 @@ function generatePyramid() {
     }
 }
 
-generatePyramid();
+// generatePyramid();
+function starpyramid(){
+    var n=5;
+    var output='';
+    for(var i=0;i<n;i++){
+        for(var j=0;j<=i;j++){
+           output+="*" + ' ';
+        }
+        output+='\n';
+    }
+    console.log(output);
+    output='';
+}
+
+// starpyramid();
+
+let n = 6;
+let string = "";
+
+for (let i = 1; i <= n; i++) {
+  // printing star
+  for (let j = 0; j < i; j++) {
+    if(i === n) {
+      string += "*";
+    }
+    else {
+      if (j == 0 || j == i - 1) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+  }
+  string += "\n";
+}
+console.log(string);
