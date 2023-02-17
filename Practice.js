7fdb3535c7de054c4f5ecc0721a8144d1e7b67ad
Navdeep <rathorenavdeep110@gmail.com>
2023-02-17 14:23:31 +0530

    Added some more programs

--- Practice.js
+++ Practice.js
@@ -32,24 +32,53 @@ function starpyramid(){
 
 // starpyramid();
 
-let n = 6;
-let string = "";
+// let n = 6;
+// let string = "";
 
-for (let i = 1; i <= n; i++) {
+// for (let i = 1; i <= n; i++) {
   // printing star
-  for (let j = 0; j < i; j++) {
-    if(i === n) {
-      string += "*";
-    }
-    else {
-      if (j == 0 || j == i - 1) {
-        string += "*";
-      }
-      else {
-        string += " ";
-      }
-    }
-  }
-  string += "\n";
+  // for (let j = 0; j < i; j++) {
+  //   if(i === n) {
+  //     string += "*";
+  //   }
+  //   else {
+  //     if (j == 0 || j == i - 1) {
+  //       string += "*";
+  //     }
+  //     else {
+  //       string += " ";
+  //     }
+  //   }
+  // }
+  // string += "\n";
+// }
+// console.log(string);
+
+// javascript function to calculate prime number ?
+// const isPrime = num => {
+//     for(let i = 2, s = Math.sqrt(num); i <= s; i++) {
+//         if(num % i === 0) return false;
+//     }
+//     return num > 1;
+// }
+
+
+
+
+
+
+// javascript function to print fibonacci series ?
+var i;
+var fib = [0, 1]; // Initialize array!
+
+for (i = 2; i <= 10; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
 }
-console.log(string);
+
+
+
+
+
