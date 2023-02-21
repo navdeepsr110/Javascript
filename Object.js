3c1173bba8a2a9b6bd28c30dd48c007f8b28951c
Navdeep <rathorenavdeep110@gmail.com>
2023-02-21 22:51:32 +0530

    Learnt Objects

---
+++ Object.js
@@ -0,0 +1,10 @@
+var student = {
+    "name": "abc",
+    rollNo: 135,
+    marks: 90,
+};
+
+// console.log(student);
+console.log(student.name);
+// Square bracket notation
+console.log(student["name"]);
