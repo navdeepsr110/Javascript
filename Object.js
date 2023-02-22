8b8dc4a4634f0551b50bf5ec265c9616a2f67aa4
Navdeep <rathorenavdeep110@gmail.com>
2023-02-22 23:39:11 +0530

    Learnt deleting properties

--- Object.js
+++ Object.js
@@ -5,11 +5,11 @@
 // };
 
 
-var student = {
+/*var student = {
     "-name": "abc",
     rollNo: 135,
     marks: 90,
-};
+};*/
 
 // console.log(student);
 
@@ -17,4 +17,18 @@ var student = {
 // So Here it gives an error
 // console.log(student.-name);
 // Square bracket notation
-console.log(student["-name"]);
+// console.log(student["-name"]);
+
+
+// Deleting property
+var student = {
+    "name":"abc",
+    rollNo: 135,
+    marks: 90,
+    "2": "two",
+};
+console.log(student);
+delete student.marks;
+console.log(student);
+delete student.rollNo;
+console.log(student);
