81862f74f46394de06c5cbd1d56f06676d8aedfe
Navdeep <rathorenavdeep110@gmail.com>
2023-03-20 23:19:29 +0530

    Added CSS and javascript

---
+++ Change_Color_And_Shape/demo.js
@@ -0,0 +1,9 @@
+var cur="square";
+
+var shape =["square","rectangle","circle","oval","triangle-up","triangle-down","triangle-topleft","triangle-bottomleft","triangle-right"];
+
+var color=["red","orange","pink","maroon","green","violet"];
+
+document.getElementById("shape").onclick = function(){
+    var rand = shape
+}
