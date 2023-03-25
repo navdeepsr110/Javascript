255df554b3416f6fdfd5c2e51f23d8572a332d8b
Navdeep <rathorenavdeep110@gmail.com>
2023-03-25 23:51:24 +0530

    Added spaces

--- Change_Color_And_Shape/demo.js
+++ Change_Color_And_Shape/demo.js
@@ -10,6 +10,7 @@ document.getElementById("shape").onclick = function(){
     cur = rand;
 }
 
+
 document.getElementById("color").onclick = function() {
     var randColor = color[Math.floor(Math.random()*color.length)];
     document.getElementById("block").style.backgroundColor = randColor;
