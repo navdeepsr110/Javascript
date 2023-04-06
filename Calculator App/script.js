34b450d543e5c5b23869892e70fb79b787c1a66e
Navdeep <rathorenavdeep110@gmail.com>
2023-04-06 23:42:29 +0530

    Calculator project

---
+++ Calculator App/script.js
@@ -0,0 +1,21 @@
+var num1=0;
+var num2=null;
+var operator=null
+
+var display = document.getElementById("display");
+var btns = document.getElementsByClassName("button");
+
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click', function(){
+     var value = this.getAttribute('data-value');
+     if(value == '+'){
+       operator = '+' ;
+       operand1 = display.innerText;
+     }
+     else if(value== '='){
+
+     }else{
+        display.innerText += value;
+     }
+    });
+}
