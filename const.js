71abfe0dd1c5f3e8028897deee381d14c4defb33
Navdeep <rathorenavdeep110@gmail.com>
2023-04-11 22:25:39 +0530

    Variables

---
+++ const.js
@@ -0,0 +1,12 @@
+console.log(a);
+var a=10;
+console.log(c);
+const c=30;
+
+
+// var a;
+// a=20;
+// console.log(a);
+// const c;
+// c=40;
+// console.log(c);
---
+++ let.js
@@ -0,0 +1,9 @@
+var a=10;
+console.log(a,b,c);
+if(a<=10){
+    var b=40;
+}
+else{
+    var c=50;
+}
+console.log(a,b,c);
