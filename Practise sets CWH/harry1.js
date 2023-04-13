b80b03ff883ae97774717aff5cdebfc84597ea23
Navdeep <rathorenavdeep110@gmail.com>
2023-04-13 23:14:31 +0530

    Cwh harry

---
+++ Practise sets CWH/harry1.js
@@ -0,0 +1,35 @@
+
+// chapter1 Q1
+/*let a = "navdeep"
+let b = 9;
+console.log(a+b);*/
+
+// chapter2 Q2
+/*let a ="Navdeep";
+console.log(typeof(a));*/
+
+// Q3
+/*const obj ={
+  name:"navdeep",
+  rno:21,
+  isPrinciple:false
+
+}
+obj = 34;*/
+
+// Q4
+/*console.log(obj);
+obj.rno=2;
+console.log(obj);
+obj.isPrinciple=true;
+console.log(obj);*/
+
+// Q5
+let dictionary = {
+    Anonymous:"A person which is not known",
+    Pessimist:"A person who has negative attitude",
+    Rascal:"A mischievous person",
+    Polymath:"A person who is profecient in many fields",
+    Polyglot:"A person who can speak more than two or three languages"
+}
+console.log(dictionary.Anonymous);
