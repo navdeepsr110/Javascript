b7a2764fe532db7600c9a16192ad8344d8483731
Navdeep <rathorenavdeep110@gmail.com>
2023-04-15 16:26:09 +0530

    Counter Clicks

--- Counter/index.js
+++ Counter/index.js
@@ -1,17 +1,28 @@
-var box = document.getElementById('box');
-var clickCount=document.getElementById('click-count');
+// var box = document.getElementById('box');
+// var clickCount=document.getElementById('click-count');
 
+// var sec=0;
+// function time(){
+//     console.log(sec);
+//     sec++;
+//     if(sec==10){
+//         //Stop
+//         clearInterval(id);
+//     }
+// }
 
+// var id = setInterval(time, 1000);
 
+var current = document.querySelector('.current');
+var next = document.querySelector('.next');
 
-var sec=0;
-function time(){
-    console.log(sec);
-    sec++;
-    if(sec==10){
-        //Stop
-        clearInterval(id);
-    }
+function startCounter(){
+    var interval = setInterval(animate,1000);
 }
 
-var id = setInterval(time, 1000);
+function animate(){
+    next.classList.add('animate');
+    setTimeout(function(){
+        next.classList.remove('animate');
+    }, 500);
+}
