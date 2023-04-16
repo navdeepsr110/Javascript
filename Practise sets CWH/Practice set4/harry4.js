bfd67f8d66b70728080113f3840a59f37587046e
Navdeep <rathorenavdeep110@gmail.com>
2023-04-16 22:48:44 +0530

    Practice set4

---
+++ Practise sets CWH/Practice set4/harry4.js
@@ -0,0 +1,28 @@
+// Q1
+/*let str ="Har\""
+console.log(str.length)*/
+
+/*const sentence = 'The quick brown fox jumps over the lazy dog.';
+const word = 'fox2';
+console.log(sentence.includes(word))
+console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);*/
+
+// Q2
+/*console.log(sentence.startsWith("fox2"));
+console.log(sentence.endsWith("dog."));*/
+
+// Q3
+/*let str='MY NAME IS NAVDEEP SINGH RATHORE';
+console.log(str.toLowerCase());*/
+
+//Q4
+/*let str = "Please give Rs 1000";
+let amount = Number.parseInt(str.slice(15)) // This is how we can change the datatype from string to number
+ let amount = str.slice(15);
+ console.log(amount);
+ console.log(typeof amount)*/
+
+ //Q5
+ /*let str = 'Navdeep';
+ str[4]='b';
+ console.log(str);*/ // str is not  changed beacause string is immutable
