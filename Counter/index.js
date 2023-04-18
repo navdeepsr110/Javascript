9672cca1ca0db426cadaba92276606eeabe15db6
Navdeep <rathorenavdeep110@gmail.com>
2023-04-18 23:36:30 +0530

    counter update

--- Counter/index.js
+++ Counter/index.js
@@ -1,29 +1,37 @@
-// var box = document.getElementById('box');
-// var clickCount=document.getElementById('click-count');
+var box = document.getElementById('box');
+var clickCount=document.getElementById('click-count');
 
-// var sec=0;
-// function time(){
-//     console.log(sec);
-//     sec++;
-//     if(sec==10){
-//         //Stop
-//         clearInterval(id);
-//     }
+var sec=0;
+function time(){
+    console.log(sec);
+    sec++;
+    if(sec==10){
+        //Stop
+        clearInterval(id);
+    }
+}
+
+var id = setInterval(time, 1000);
+
+
+
+
+
+
+
+
+
+// var current = document.querySelector('.current');
+// var next = document.querySelector('.next');
+
+
+// function startCounter(){
+//     var interval = setInterval(animate,1000);
 // }
 
-// var id = setInterval(time, 1000);
-
-var current = document.querySelector('.current');
-var next = document.querySelector('.next');
-
-
-function startCounter(){
-    var interval = setInterval(animate,1000);
-}
-
-function animate(){
-    next.classList.add('animate');
-    setTimeout(function(){
-        next.classList.remove('animate');
-    }, 500);
-}
+// function animate(){
+//     next.classList.add('animate');
+//     setTimeout(function(){
+//         next.classList.remove('animate');
+//     }, 500);
+// }
