21550601b794cceb7eebfbad652e3aa46c4e8ce9
Navdeep <rathorenavdeep110@gmail.com>
2023-04-29 19:06:06 +0530

    Completed

--- Counter/index.js
+++ Counter/index.js
@@ -1,37 +1,60 @@
-var box = document.getElementById('box');
-var clickCount=document.getElementById('click-count');
+var countInterval;
 
-var sec=0;
-function time(){
-    console.log(sec);
-    sec++;
-    if(sec==10){
-        //Stop
-        clearInterval(id);
+
+function startCounter() {
+
+    var number = parseInt(document.getElementById("number").value);
+
+    if (isNaN(number)) {
+        alert("Please enter a number");
+        clearInterval(countInterval);   // This is important for the condition when a counter is running and you entered a wrong input for a new counter
+        return;
     }
+    if (number < 1 || number > 9) {
+        alert("Range out of bounds");
+        clearInterval(countInterval);
+        return;
+    }
+
+    var currentNo = document.querySelector(".counter .current");
+    var nextNo = document.querySelector(".counter .next");
+    var count = 0;
+
+    // If user clicks on 'Start Counter' button again - remove this function and below 2 lines if you don't consider this situation
+    resetNumbers(currentNo, nextNo);
+    
+    // Clears the previous interval that was running
+    clearInterval(countInterval);
+
+    countInterval = setInterval(function () {
+        if (count === number) {
+            clearInterval(countInterval);
+            alert("Counter has stopped");
+            return;
+        }
+        increaseCount(currentNo, nextNo);
+        count++;
+    }, 1000);
+
 }
- 
-var id = setInterval(time, 1000);
 
 
 
+function resetNumbers(currentNo, nextNo, end) {
+    currentNo.innerText = 0;
+    nextNo.innerText = 1;
+}
 
 
 
+function increaseCount(currentNo, nextNo) {
 
+    nextNo.classList.add("animate");
 
+    setTimeout(function () {
+        currentNo.innerText = nextNo.innerText;
+        nextNo.classList.remove("animate");
+        nextNo.innerText = parseInt(nextNo.innerText) + 1;
+    }, 500);
 
-// var current = document.querySelector('.current');
-// var next = document.querySelector('.next');
-
-
-// function startCounter(){
-//     var interval = setInterval(animate,1000);
-// }
-
-// function animate(){
-//     next.classList.add('animate');
-//     setTimeout(function(){
-//         next.classList.remove('animate');
-//     }, 500);
-// }
+}
