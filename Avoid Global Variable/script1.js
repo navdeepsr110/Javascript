d895d0d20e6366240301d57daad588ae10aab822
Navdeep <rathorenavdeep110@gmail.com>
2023-05-22 16:07:19 +0530

    Avoid Global declaration of a variable

---
+++ Avoid Global Variable/script1.js
@@ -0,0 +1,11 @@
+// var name = "File1";
+
+// setTimeout(function(){
+//     console.log(name);
+// }, 1000);
+
+// Apart from this we can also use block scope
+{
+    let fileName = "File1"
+    console.log(finlename);
+}
